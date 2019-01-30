4c13f290f8d9f59e6e26245429862263af117de3
Jonathan Heinz <jonathan.heinz@aol.com>
2019-01-30 14:48:27 -0800

    added tests for controls component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,30 @@
 // Test away!
+
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+
+  it('should disable lock button when gate is open', () => {
+
+    const { getByText } = render(<Controls />);
+
+    const lockBtn = getByText(/Lock Gate/i);
+
+    expect(lockBtn.disabled).toBe(true);
+
+  });
+
+  it('should disable open button when gate is locked', () => {
+
+    const { getByText } = render(<Controls locked={true} />);
+
+    const openBtn = getByText(/Open Gate/i);
+
+    expect(openBtn.disabled).toBe(true);
+
+  });
+
+});
